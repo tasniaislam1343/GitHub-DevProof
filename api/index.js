a82c7018ec2a5ef9a16ac2c0df05333ffdd1bad4
samanta <tasniaislam2004@icloud.com>
2026-07-02 19:16:31 +0600

    Fix Vercel deployment: add vercelignore to include Backend node_modules, simplify api entry

--- api/index.js
+++ api/index.js
@@ -1,9 +1,3 @@
-const path = require('path');
-
-if (process.env.VERCEL) {
-  require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-}
-
 const app = require('../Backend/src/server');
 
 module.exports = app;
